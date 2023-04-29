76794a06a7e511d8a2c15fe02800415b1092c8ce
ViViV9 <vivianaverachtert@gmail.com>
2023-04-29 17:56:14 -0300

    edicion de persona

--- src/main/java/com/myportfolio/argprogviviana/controller/CPersona.java
+++ src/main/java/com/myportfolio/argprogviviana/controller/CPersona.java
@@ -4,33 +4,39 @@ import com.myportfolio.argprogviviana.entity.Persona;
 import com.myportfolio.argprogviviana.service.PersonaDTO;
 import com.myportfolio.argprogviviana.service.SPersona;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-//@RequestMapping("persona")
+@RequestMapping("persona")
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://portfoliovivianaverachtert.web.app")
 public class CPersona {
     @Autowired
     SPersona personaServ;
 
-    ///@GetMapping("/buscar/{id}") esto lo comenté para probar si me funcionan los cambios
-    @GetMapping("/persona/{id}")
+    @GetMapping("/lista")
+    @ResponseBody
+    public List<Persona> verPersonas(){
+        return personaServ.verPersonas();
+    }
+
+    @GetMapping("/buscar/{id}")
     @ResponseBody
     public Persona buscarPersona(@PathVariable int id) {
         return personaServ.buscarPersona(id);
     }
 
-    @PutMapping("/persona")
+    @PutMapping("/editar")
     public void editarPersona(@RequestBody Persona pers) {
         personaServ.editarPersona(pers);
+    }
+
+    @PostMapping("/crear")
+    public String crearPersona(@RequestBody Persona pers){
+        personaServ.crearPersona(pers);
+        return "La persona fue creada correctamente";
     }
 
     @PostMapping("/login")
@@ -38,18 +44,6 @@ public class CPersona {
     public PersonaDTO login(@RequestBody Persona pers){
         return personaServ.login(pers.getEmail(), pers.getPassword());
     }
-
-    ///@GetMapping("/lista")
-    ///@ResponseBody
-    ///public List<Persona> verPersonas(){
-        ///return personaServ.verPersonas();
-    ///}
-
-    ///@PostMapping("/crear")
-    /// public String crearPersona(@RequestBody Persona pers){
-    ///    personaServ.crearPersona(pers);
-    ///    return "La persona fue creada correctamente";
-    ///}
 
     ///@DeleteMapping("/borrar/{id}")
     ///public String borrarPersona(@PathVariable int id){
--- src/main/java/com/myportfolio/argprogviviana/service/SPersona.java
+++ src/main/java/com/myportfolio/argprogviviana/service/SPersona.java
@@ -6,16 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class SPersona{
     @Autowired
      private RPersona personaRepo;
 
+    public List<Persona> verPersonas(){
+        return  personaRepo.findAll();
+    }
+
     public Persona buscarPersona(Integer id) {
         return personaRepo.findById(id).orElse(null);
     }
+
     public void editarPersona(Persona perso){
+        personaRepo.save(perso);
+    }
+
+    public void crearPersona(Persona perso){
         personaRepo.save(perso);
     }
 
@@ -24,14 +35,6 @@ public class SPersona{
         PersonaDTO personaDTO = new PersonaDTO(pers.getId(), pers.getNombre(), pers.getApellido(), pers.getTitulo(), pers.getFoto_perfil(), pers.getTextsobre_mi(), pers.getTextsobre_mi2(), pers.getTextsobre_mi3(), pers.getCv());
         return personaDTO;
     }
-
-    ///public List<Persona> verPersonas(){
-    /// return  personaRepo.findAll();
-    ///}
-
-    ///public void crearPersona(Persona perso){
-     ///   personaRepo.save(perso);
-    ///}
 
     ///public void borrarPersona(int id){
        /// personaRepo.deleteById(id);
